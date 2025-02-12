ce1dbe154c36d5443d925570b0f439190d20b0ac
keneogu <kc101com@gmail.com>
2025-02-12 22:50:59 +0100

    Add the Lecture model

---
+++ src/main/java/com/kencode/jpa/model/Lecture.java
@@ -0,0 +1,25 @@
+package com.kencode.jpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "lectures")
+public class Lecture {
+
+  @Id
+  @GeneratedValue
+  private Integer id;
+
+  private String name;
+}
