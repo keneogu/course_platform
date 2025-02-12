a2e5f3142971954e2e9934929f9632cf1a761751
keneogu <kc101com@gmail.com>
2025-02-12 22:49:14 +0100

    Create the Author model

---
+++ src/main/java/com/kencode/jpa/model/Author.java
@@ -0,0 +1,49 @@
+package com.kencode.jpa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_sequence")
+    @SequenceGenerator(name = "author_sequence", sequenceName = "author_sequence", allocationSize = 1)
+    private Integer id;
+
+    @Column(
+            name = "f_name"
+    )
+    private String firstName;
+
+    private String lastName;
+
+    @Column(
+            unique = true,
+            nullable = false
+    )
+    private String email;
+
+    private Integer age;
+
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
+}
