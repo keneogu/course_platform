6c1ee18469a2413845f30d0c98e2635393ede9c8
keneogu <kc101com@gmail.com>
2025-02-12 22:50:26 +0100

    Add the course model

---
+++ src/main/java/com/kencode/jpa/model/Course.java
@@ -0,0 +1,27 @@
+package com.kencode.jpa.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "courses")
+public class Course {
+
+  @Id
+  @GeneratedValue
+  private Integer id;
+
+  private String name;
+
+  private String description;
+}
