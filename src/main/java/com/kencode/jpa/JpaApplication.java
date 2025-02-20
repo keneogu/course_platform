02ab36669ff4e9e75c7667bf7666e282c1dfa55a
keneogu <kc101com@gmail.com>
2025-02-20 11:50:15 +0100

    Create findByFirstName query and add names using javaFaker

--- src/main/java/com/kencode/jpa/JpaApplication.java
+++ src/main/java/com/kencode/jpa/JpaApplication.java
@@ -1,12 +1,12 @@
 package com.kencode.jpa;
 
-//import com.kencode.jpa.model.Author;
-//import com.kencode.jpa.repositories.AuthorRepository;
-////import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.CommandLineRunner;
+import com.github.javafaker.Faker;
+import com.kencode.jpa.model.Author;
+import com.kencode.jpa.repositories.AuthorRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -15,16 +15,18 @@ public class JpaApplication {
 		SpringApplication.run(JpaApplication.class, args);
 	}
 
-//	@Bean
-//	public CommandLineRunner commandLineRunner(AuthorRepository repository) {
-//		return args -> {
-//			var author = Author.builder()
-//					.firstName("kene")
-//					.lastName("Ogu")
-//					.email("contact@keneogu.com")
-//					.age(33)
-//					.build();
-//			repository.save(author);
-//		};
-//	}
+	@Bean
+	public CommandLineRunner commandLineRunner(AuthorRepository repository) {
+		return args -> {
+			for (int i = 0; i < 50; i++) {
+				Faker faker = new Faker();
+				var author = Author.builder().firstName(faker.name().firstName())
+						.lastName(faker.name().lastName())
+						.age(faker.number().numberBetween(19, 50))
+						.email("contact" + i + "@keneogu.com")
+						.build();
+				repository.save(author);
+			}
+		};
+	}
 }
--- src/main/java/com/kencode/jpa/repositories/AuthorRepository.java
+++ src/main/java/com/kencode/jpa/repositories/AuthorRepository.java
@@ -3,5 +3,8 @@ package com.kencode.jpa.repositories;
 import com.kencode.jpa.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+  List<Author> findAllByFirstName(String fn);
 }
