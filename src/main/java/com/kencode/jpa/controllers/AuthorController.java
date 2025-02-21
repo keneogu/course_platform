93460df39617a6b7165e1b92ff3bea71c32a5aa3
keneogu <kc101com@gmail.com>
2025-02-22 00:23:12 +0100

    implement get an Author by id controller and service methods

--- src/main/java/com/kencode/jpa/controllers/AuthorController.java
+++ src/main/java/com/kencode/jpa/controllers/AuthorController.java
@@ -4,6 +4,7 @@ import com.kencode.jpa.model.Author;
 import com.kencode.jpa.services.AuthorServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,11 @@ public class AuthorController {
   @GetMapping("/authors")
   public List<Author> getAuthors() {
     return authorServices.getAuthors();
+  }
+
+  @GetMapping("/authors/{id}")
+  public Optional<Author> getAuthor(@PathVariable("id") Integer id) {
+    return authorServices.getAuthor(id);
   }
 
 }
--- src/main/java/com/kencode/jpa/services/AuthorServices.java
+++ src/main/java/com/kencode/jpa/services/AuthorServices.java
@@ -4,7 +4,9 @@ package com.kencode.jpa.services;
 import com.kencode.jpa.model.Author;
 import com.kencode.jpa.repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,8 +21,8 @@ public class AuthorServices {
     return authorRepository.findAll();
   }
 
-//  public Optional<Author> getAuthor(int authorId) {
-//    return authorRepository.findById(authorId);
-//  }
+  public Optional<Author> getAuthor(Integer id) {
+    return authorRepository.findById(id);
+  }
 
 }
