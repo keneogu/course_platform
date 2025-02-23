ebcaa087f497a9c017152df3f06fb1dfd36d9ac4
keneogu <kc101com@gmail.com>
2025-02-23 22:53:16 +0100

    Add the cascade to update the authors when creating a course

--- src/main/java/com/kencode/jpa/model/Course.java
+++ src/main/java/com/kencode/jpa/model/Course.java
@@ -19,7 +19,7 @@ public class Course extends BaseEntity{
 
   private String description;
 
-  @ManyToMany
+  @ManyToMany(cascade = CascadeType.ALL)
   @JoinTable(
       name = "authors_courses",
       joinColumns = {
