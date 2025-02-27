d8032800b0481f474703ba5dcaaa4da4ba36d29b
keneogu <kc101com@gmail.com>
2025-02-27 17:31:00 +0100

    Create Section Repository interface

---
+++ src/main/java/com/kencode/jpa/repositories/SectionRepository.java
@@ -0,0 +1,8 @@
+package com.kencode.jpa.repositories;
+
+import com.kencode.jpa.model.Section;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SectionRepository extends JpaRepository<Section, Integer> {
+
+}
