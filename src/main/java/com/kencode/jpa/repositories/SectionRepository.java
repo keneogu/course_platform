8a66cd5ba11c4d9beaba3aea4694d52f26273568
keneogu <kc101com@gmail.com>
2025-02-28 19:09:27 +0100

    Add findByCourseId in SectionRepository.

--- src/main/java/com/kencode/jpa/repositories/SectionRepository.java
+++ src/main/java/com/kencode/jpa/repositories/SectionRepository.java
@@ -3,6 +3,8 @@ package com.kencode.jpa.repositories;
 import com.kencode.jpa.model.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SectionRepository extends JpaRepository<Section, Integer> {
+import java.util.List;
 
+public interface SectionRepository extends JpaRepository<Section, Integer> {
+  List<Section> findByCourseId(Integer courseId);
 }
