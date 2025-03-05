c43dc0025f54e73fc658287f688676ec4321647f
keneogu <kc101com@gmail.com>
2025-03-05 17:36:49 +0100

    Implement delete resource method which removes the forign key from lecture

--- src/main/java/com/kencode/jpa/repositories/ResourceRepository.java
+++ src/main/java/com/kencode/jpa/repositories/ResourceRepository.java
@@ -2,8 +2,16 @@ package com.kencode.jpa.repositories;
 
 import com.kencode.jpa.model.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResourceRepository extends JpaRepository<Resource, Integer> {
+
+  @Modifying
+  @Query("DELETE FROM Resource r WHERE r.id = :resId AND r.lecture.id = :lectureId")
+  void deleteResource(@Param("lectureId") Integer lectureId, @Param("resId") Integer resId);
+
 }
--- src/main/java/com/kencode/jpa/services/ResourceServices.java
+++ src/main/java/com/kencode/jpa/services/ResourceServices.java
@@ -1,8 +1,8 @@
 package com.kencode.jpa.services;
 
 import com.kencode.jpa.model.*;
-import com.kencode.jpa.repositories.LectureRepository;
-import com.kencode.jpa.repositories.ResourceRepository;
+import com.kencode.jpa.repositories.*;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,6 +35,27 @@ public class ResourceServices {
 
   public List<Resource> getAllResources() {
     return resourceRepository.findAll();
+  }
+
+  @Transactional
+  public void deleteResource(Integer lectureId, Integer resId) {
+
+    Lecture lecture = lectureRepository.findById(lectureId)
+        .orElseThrow(() -> new RuntimeException("lecture not found"));
+
+    Resource resource = resourceRepository.findById(resId).orElseThrow(() -> new RuntimeException("Resource video not found"));
+
+    // Check if the lecture belongs to the specified course or if the lecture belongs to a specified lecture
+    if (!resource.getLecture().getId().equals(lectureId) || !lecture.getResource().getId().equals(resId)) {
+      throw new RuntimeException("Resource/Lecture does not belong to the specified Resource/Lecture");
+    }
+
+    if (resource.getLecture() != null) {
+      resource.getLecture().setResource(null);
+      lectureRepository.save(resource.getLecture()); // Update the lecture
+    }
+
+    resourceRepository.delete(resource);
   }
 
 }
